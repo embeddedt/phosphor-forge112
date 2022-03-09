d9b6bba2db3f0c7b2230d9abad1ab95eb6e4e183
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-03-09 12:54:30 -0500

    Fix compatiblity with Mixin 0.8.3
    Previously, ModifyVariable mixins did not respect their slice: https://github.com/SpongePowered/Mixin/issues/429
    
    These mixins have been rewritten to avoid depending on a slice.
    
    This probably fixes https://github.com/LoliKingdom/MixinBooter/issues/12,
    https://github.com/LXGaming/MixinBootstrap/issues/52, and others.

--- src/main/java/me/jellysquid/mods/phosphor/mixins/lighting/common/MixinChunk$Vanilla.java
+++ src/main/java/me/jellysquid/mods/phosphor/mixins/lighting/common/MixinChunk$Vanilla.java
@@ -1,16 +1,17 @@
 package me.jellysquid.mods.phosphor.mixins.lighting.common;
 
 import me.jellysquid.mods.phosphor.mod.world.lighting.LightingHooks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Chunk.class)
 public abstract class MixinChunk$Vanilla {
@@ -61,18 +62,6 @@ public abstract class MixinChunk$Vanilla {
             ),
             index = 13,
             name = "flag",
-            slice = @Slice(
-                    from = @At(
-                            value = "FIELD",
-                            target = "Lnet/minecraft/world/chunk/Chunk;storageArrays:[Lnet/minecraft/world/chunk/storage/ExtendedBlockStorage;",
-                            ordinal = 1
-                    ),
-                    to = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/chunk/storage/ExtendedBlockStorage;set(IIILnet/minecraft/block/state/IBlockState;)V"
-                    )
-
-            ),
             allow = 1
     )
     private boolean setBlockStateInjectGenerateSkylightMapVanilla(boolean generateSkylight) {
@@ -80,68 +69,20 @@ public abstract class MixinChunk$Vanilla {
     }
 
     /**
-     * Modifies variable k1 before the conditional which decides to propagate skylight as to prevent it from
-     * ever evaluating as true
-     *
-     * @author Angeline
+     * Prevent propagateSkylightOcclusion from being called.
+     * @author embeddedt
      */
-    @ModifyVariable(
-            method = SET_BLOCK_STATE_VANILLA,
-            at = @At(
-                    value = "LOAD",
-                    ordinal = 0
-            ),
-            index = 11,
-            name = "k1",
-            slice = @Slice(
-                    from = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/chunk/Chunk;relightBlock(III)V",
-                            ordinal = 1
-                    ),
-                    to = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/chunk/Chunk;propagateSkylightOcclusion(II)V"
-                    )
-
-            ),
-            allow = 1
-    )
-    private int setBlockStatePreventPropagateSkylightOcclusion1(int generateSkylight) {
-        return WIZARD_MAGIC;
+    @Redirect(method = SET_BLOCK_STATE_VANILLA, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;propagateSkylightOcclusion(II)V"))
+    private void doPropagateSkylight(Chunk chunk, int i1, int i2) {
+        /* No-op, we don't want skylight propagated */
     }
 
     /**
-     * Modifies variable j1 before the conditional which decides to propagate skylight as to prevent it from
-     * ever evaluating as true.
-     *
-     * @author Angeline
+     * Prevent getLightFor from being called.
+     * @author embeddedt
      */
-    @ModifyVariable(
-            method = SET_BLOCK_STATE_VANILLA,
-            at = @At(
-                    value = "LOAD",
-                    ordinal = 1
-            ),
-            index = 14,
-            name = "j1",
-            slice = @Slice(
-                    from = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/chunk/Chunk;relightBlock(III)V",
-                            ordinal = 1
-                    ),
-                    to = @At(
-                            value = "INVOKE",
-                            target = "Lnet/minecraft/world/chunk/Chunk;propagateSkylightOcclusion(II)V"
-                    )
-
-            ),
-            allow = 1
-    )
-    private int setBlockStatePreventPropagateSkylightOcclusion2(int generateSkylight) {
-        return WIZARD_MAGIC;
+    @Redirect(method = SET_BLOCK_STATE_VANILLA, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;getLightFor(Lnet/minecraft/world/EnumSkyBlock;Lnet/minecraft/util/math/BlockPos;)I"))
+    private int getFakeLightFor(Chunk chunk, EnumSkyBlock skyBlock, BlockPos blockPos) {
+        return 0;
     }
-
-    private static final int WIZARD_MAGIC = 694698818;
 }
